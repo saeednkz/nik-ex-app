f134a1b899d41f9c091759e3c2f03baee9fff7fb
saeednkz <saeed.vali87@gmail.com>
2025-07-30 07:46:13 +0330

    debug sheet5

--- src/App.jsx
+++ src/App.jsx
@@ -43,8 +43,8 @@ const firebaseConfig = {
 // ====================================================================================
 // =========================== GOOGLE API CONFIGURATION ===============================
 // ====================================================================================
-const GOOGLE_API_KEY = "AIzaSyBtvPsfnNLCXEGDToRArSwIr-qfa63GuLY"; // FIXED: Using the correct API key from Firebase config
-const GOOGLE_CLIENT_ID = "522998435883-mvbn4472bcki6gp9uut9lbehojpo5p62.apps.googleusercontent.com"; 
+const GOOGLE_API_KEY = "AIzaSyDe2ZgPy488Ha2wMFnvHM9upHXxZXHL43E"; 
+const GOOGLE_CLIENT_ID = "522998435883-uojc6gmpo0k79d0s7se0iu2bumh6l796.apps.googleusercontent.com"; 
 const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
 const SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
 // ====================================================================================
